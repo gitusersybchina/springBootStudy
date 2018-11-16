f19a7265f7038d69d97027accc78446230d30e48
13257 <yinbo.shi@hand-china.com>
2018-11-16 10:18:21 +0800

    "fix:随便改改;"

---
+++ springboot-jpa/src/main/java/com/xiaobo/example/SpringBootJpaApplication.java
@@ -0,0 +1,15 @@
+package com.xiaobo.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Administrator
+ */
+@SpringBootApplication
+public class SpringBootJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJpaApplication.class, args);
+    }
+}
